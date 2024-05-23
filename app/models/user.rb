ec1d7af009a1530700f99d145f4e8338b9a2ba36
Ryutaro22 <y.ryutaro0423@gmail.com>
2024-05-23 18:35:55 +0900

    新規登録、ログイン、ログアウト等の実装

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+
+end
