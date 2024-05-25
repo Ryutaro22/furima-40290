00822572b69928009280dbb04309f53320ef05be
Ryutaro22 <y.ryutaro0423@gmail.com>
2024-05-25 18:03:53 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
   validates :last_name, presence: true
@@ -11,5 +10,4 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
-
 end
