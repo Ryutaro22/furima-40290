825bbd1e7025c9e73f58627f3609736a2d5c9f5c
Ryutaro22 <y.ryutaro0423@gmail.com>
2024-05-26 16:03:32 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めてください'
+
+  with_option presence: ture, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :nickname, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
 end
