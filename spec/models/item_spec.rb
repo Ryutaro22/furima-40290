bdd7aed2e65b6e8374be652c4d0d1be800d96925
Ryutaro22 <y.ryutaro0423@gmail.com>
2024-06-03 18:54:52 +0900

    商品出品機能

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe 'アイテムの保存' do
+    context 'アイテムが出品できる場合' do
+      it '出品に必須な情報が存在すれば投稿できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context 'アイテムが出品できない場合' do
+      it '商品名が空では投稿できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it '商品説明が空では出品できない' do
+        @item.item_info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item info can't be blank")
+      end
+
+      it '画像が空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'カテゴリーが選択されていなければ出品できない' do
+        @item.category = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '状態が選択されていなければ出品できない' do
+        @item.item_condition = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item Condition can't be blank")
+      end
+
+      it '配送料が選択されていなければ出品できない' do
+        @item.postage_payer = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payer can't be blank")
+      end
+
+      it '発送日が選択されていなければ出品できない' do
+        @item.shipping_period = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping period can't be blank")
+      end
+
+      it '価格が選択されていなければ出品できない' do
+        @item.item_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item Price can't be blank")
+      end
+
+      it '価格が300以下では出品できない' do
+        @item.item_price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price must be greater than 299")
+      end
+
+      it '価格が9999999以上では出品できない' do
+        @item.item_price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price must be less than 10000000")
+      end
+
+      it '価格が半角数値以外では出品できない' do
+        @item.item_price = '５５５５５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price is not a number")
+      end
+
+      it 'ユーザーが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
+end
